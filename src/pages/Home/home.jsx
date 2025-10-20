dd25b4067ebf5361d84ef23667a5efc6ef6ef7c8
Katlego Morwamohube <129260867+MartuorDysiphon@users.noreply.github.com>
2025-10-20 23:37:36 +0200

    Update home.jsx

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,43 +1,42 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
 import './home.css';
-function Home () {
+
+function Home() {
     return ( 
         <div className="home">
             <header className='home__header'>
                 <div className="section__container header__container" id="home">
-                <p>Unique - Amazing - Safe</p>
-                <h1>The Simplicity of a Home Away From Home <br /><span>_</span></h1>
+                    <p>Unique - Amazing - Safe</p>
+                    <h1>The Simplicity of a Home Away From Home <br /><span>_</span></h1>
                 </div>
             </header>
-              <div className="section__container booking__container">
-                <form action="/" className="booking__form">
-                <div className="input__group">
-                    <div>
-                    <p className="form__title">Application Gategories</p>
+            <div className="section__container booking__container">
+                <form className="booking__form">
+                    <div className="input__group">
+                        <div>
+                            <p className="form__title">Application Categories</p>
+                        </div>
                     </div>
-                </div>
-                <div className="input__group">
-                    <span><i className="ri-calendar-2-fill"></i></span>
-                    <div>
+                    <div className="input__group">
+                        <span><i className="ri-calendar-2-fill"></i></span>
+                        <div>
+                            <Link to="/apply" className="btn1">Semester Application</Link>
+                        </div>
                     </div>
-                    <Link to="/apply" className="btn1">Semester Application</Link>
-                </div>
-                <div className="input__group">
-                    <span><i className="ri-calendar-2-fill"></i></span>
-                    <div>
-                    <Link to="/apply" className="btn1">Year Application</Link>
+                    <div className="input__group">
+                        <span><i className="ri-calendar-2-fill"></i></span>
+                        <div>
+                            <Link to="/apply" className="btn1">Year Application</Link>
+                        </div>
                     </div>
-                </div>
-                <div className="input__group input__btn">
-                    <Link to="/explore" className="gallery__btn">Explore</Link>
-                </div>
+                    <div className="input__group input__btn">
+                        <Link to="/explore" className="gallery__btn">Explore</Link>
+                    </div>
                 </form>
             </div>
         </div>
-    
     );
 }
 
-export default Home ;
+export default Home;
